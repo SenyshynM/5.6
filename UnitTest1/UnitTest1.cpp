0954ea42e1d63e2bc96e994312646d1a49cba6ef
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2020-12-14 13:12:45 +0200

    test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.6/5.6.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int t, s;
+			t = 3;
+			s = 4;
+			double m = (f(t, s, 2) + f(1, s + t, t - s)) / (1 + pow(f(1, 1, t * t + s * s), 2));
+			double expected = 6.97546;
+			Assert::AreEqual(expected, m, 0.0001);
+		}
+	};
+}

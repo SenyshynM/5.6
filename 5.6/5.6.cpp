bcec1626f99a47313c966c6f09f83c4284635bb6
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2020-12-14 13:06:46 +0200

    1

---
+++ 5.6/5.6.cpp
@@ -0,0 +1,23 @@
+﻿// Lab 5.6
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double a, const double b, const double c); // прототип
+
+int main()
+{
+	double t, s;
+
+	cout << "t = "; cin >> t;
+	cout << "s = "; cin >> s;
+
+	double m = (f(t, s, 2) + f(1, s + t, t - s)) / (1 + pow(f(1, 1, t * t + s * s), 2));
+
+	cout << "m = " << m << endl;
+	return 0;
+}
+
+double f(const double a, const double b, const double c) // визначення
+{
+	return (a + b + c) / (pow(sin(a * b), 2) + c * c);
+}
